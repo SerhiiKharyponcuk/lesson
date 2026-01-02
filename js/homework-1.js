b0da5c8aad61f4f5c4280a3bbbe2621b84106e04
SerhiiKharyponcuk <hariponcuksergej@gmail.com>
2026-01-02 16:25:33 +0100

    hw-1-js

---
+++ js/homework-1.js
@@ -0,0 +1,38 @@
+// 1. age
+let age = 30;
+console.log(age);
+
+// 2. name
+let name = "Serhii";
+console.log(name);
+
+// 3. isStudent
+let isStudent = false;
+console.log(isStudent);
+
+// 4. myString
+let myString = "Навчання — це інвестиція в майбутнє.";
+console.log(myString);
+
+// 5. myNumber + 10
+let myNumber = 5;
+myNumber = myNumber + 10;
+console.log(myNumber);
+
+// 6. myNull
+let myNull = null;
+console.log(myNull);
+
+// 7. prompt + alert + typeof
+let userName = prompt("Введіть своє ім’я");
+alert("Ваше ім’я: " + userName);
+console.log(typeof userName);
+
+// 8. confirm
+let userAnswer = confirm("Ви дійсно хочете покинути сторінку?");
+console.log(userAnswer);
+
+// 9. alert + confirm
+alert("Увага! Дія може бути небезпечною.");
+let confirmAction = confirm("Ви впевнені, що хочете продовжити?");
+console.log(confirmAction);
